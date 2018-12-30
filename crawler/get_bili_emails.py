b4c669c1b66e34c2aba4662e61ade2d08a54c2a8
ran <ran@qq.com>
2018-12-30 16:34:10 +0800

    获取bilibili视频评论中的emails

---
+++ crawler/get_bili_emails.py
@@ -0,0 +1,33 @@
+# _*_ coding:utf-8 _*_
+# @Author    :ran
+# @time      :2018-12-27 06:41
+# @File      :get_bili_emails.py
+# @Software  :PyCharm
+
+from selenium import webdriver
+import time
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
+
+# 不显示浏览器后台静默加载
+chrome_options = Options()
+chrome_options.add_argument("--headless")       # define headless
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+driver.get("https://www.bilibili.com/video/av21666145")
+
+# 解决页面懒加载 执行通过执行javascript将页面滚动到指定元素
+targetElem = driver.find_element_by_xpath("//div[@id='comment']")
+driver.execute_script("arguments[0].scrollIntoView();", targetElem)
+
+
+for i in range(8):
+    time.sleep(2)
+    driver.find_element_by_link_text(u'下一页').click()
+    time.sleep(2)
+    html = driver.page_source
+    soup = BeautifulSoup(html, features="html.parser")
+    print(soup.findAll('p', {'class': 'text'}))
+
+driver.close()
+
