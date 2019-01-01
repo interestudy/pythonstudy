9bf6036899fe262e90c167cbd595e855c4ef3e9e
ran <ran@qq.com>
2019-01-01 16:44:41 +0800

    把email添加到列表

--- crawler/get_bili_emails.py
+++ crawler/get_bili_emails.py
@@ -21,22 +21,30 @@ driver.get("https://www.bilibili.com/video/av21666145")
 targetElem = driver.find_element_by_xpath("//div[@id='comment']")
 driver.execute_script("arguments[0].scrollIntoView();", targetElem)
 
+emails = list()
 
 # 循环下一页
 for i in range(8):
-    time.sleep(2)
-    print(i)
+    time.sleep(5)   # 停止5s等待网络加载
+    # 跳过第一页
     if i is not 0:
         driver.find_element_by_link_text(u'下一页').click()
-        time.sleep(2)
+        time.sleep(5)
+
     html = driver.page_source
     soup = BeautifulSoup(html, features="html.parser")
     ps = soup.findAll('p', {'class': 'text'})
-    # 循环取出列表内容
-    for j in range(len(ps)):
-        p_text = ps[j].get_text()
-        pattern = re.compile('[a-zA-Z0-9*]+@[a-zA-Z]+\.com')
-        print(pattern.findall(p_text))
 
+    # 遍历列表
+    for p in ps:
+        p_text = p.get_text()
+        # 用正则表达式匹配并取出评论中的邮箱
+        pattern = re.compile('[a-zA-Z0-9*]+@[a-zA-Z0-9]+\.com')
+        email = pattern.findall(p_text)
+        if email is not None:  # 有些评论没有邮箱 跳过添加到列表
+            emails.extend(email)
+
+
+print(emails)
 driver.close()
 
