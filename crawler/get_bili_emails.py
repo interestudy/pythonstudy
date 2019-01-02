b508a10d7836722a7e6d3e2ce5942274b5d7de93
ran <ran@qq.com>
2019-01-03 06:59:04 +0800

    添加注释

--- crawler/get_bili_emails.py
+++ crawler/get_bili_emails.py
@@ -9,6 +9,17 @@ import re
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+'''
+曾经我在bilibili上面发了个视频， 很多朋友留下邮箱需要曲谱，
+不想一页一页的手动复制，就尝试着用python来解决问题，
+
+1.用selenium 控制浏览器请求，
+2.解决页面懒加载的问题
+3.beautiful soup来获取页面标签
+4.用了一个简单的xpath 条件查找
+5.邮箱筛选的正则表达式
+6.保存为csv格式的文件
+'''
 
 
 # 启动selenium 不显示浏览器后台静默加载
@@ -52,7 +63,7 @@ def get_p(driver):
     return ps
 
 
-# 循环下一页
+# 循环下一页 取出所有email
 def net_page(driver, emails):
     for i in range(8):
         print('正在执行完第{}页,请稍等......'.format(i))
