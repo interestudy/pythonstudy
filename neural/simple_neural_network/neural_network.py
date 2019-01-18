ebdaf7d78659a2d3d51236c12bc41e2a4d767d7c
ran <ran@qq.com>
2019-01-18 11:26:34 +0800

    优化结构

---
+++ neural/simple_neural_network/neural_network.py
@@ -0,0 +1,60 @@
+# _*_ coding:utf-8 _*_
+# @Author    :ran
+# @time      :2019-01-08 09:35
+# @File      :simple_neural_network.py
+# @Software  :PyCharm
+import numpy as np
+import scipy.special
+'''Python基础知识编写的神经网络模型'''
+
+
+class NeuralNetwork:
+
+    # 初始化参数
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+
+        # 初始化权重
+        self.wih = np.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+        self.who = np.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+        self.lr = learningrate
+
+        self.activation_function = lambda x: scipy.special.expit(x)
+        pass
+
+    # 训练神经网络
+    def train(self, inputs_list, targets_list):
+        inputs = np.array(inputs_list, ndmin=2).T
+        targets = np.array(targets_list, ndmin=2).T
+
+        hidden_inputs = np.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        final_inputs = np.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
+        output_errors = targets - final_outputs
+        hidden_errors = np.dot(self.who.T, output_errors)
+
+        # 更新权重 把误差和权重的斜率作为权重的变化值
+        self.who += self.lr * np.dot(
+            (output_errors * final_outputs * (1.0 - final_outputs)), np.transpose(hidden_outputs))
+        self.wih += self.lr * np.dot(
+            (hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), np.transpose(inputs))
+        print(output_errors[0])
+        pass
+
+    # 查询
+    def query(self, inputs_list):
+        inputs = np.array(inputs_list, ndmin=2).T
+
+        hidden_inputs = np.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        final_inputs = np.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
+
